675561bdf32d8f9af0a62ec737555f45c618cbf0
Viktor Dorokhov <waveapologiesng@gmail.com>
2021-12-22 22:37:03 +0300

    completed all the necessary features

--- main.py
+++ main.py
@@ -42,28 +42,28 @@ def create_database():
     def creation():
         if name.get():
             if name.get() in db_list:
-                show_error("Such a database already exists", newWindow)
+                show_error("Such a database already exists", create_window)
             else:
                 try:
                     query = '''select create_db('{}');'''.format(name.get())
                     cursor.execute(query)
                     db_list.append(name.get())
                     db_listbox.insert(END, name.get())
-                    newWindow.destroy()
+                    create_window.destroy()
                 except psycopg2.errors.SqlclientUnableToEstablishSqlconnection:
                     print("DEBUG:: Error while creating database: сould not establish connection")
-                    show_error("Could not establish connection", newWindow)
+                    show_error("Could not establish connection", create_window)
         else:
-            show_error("The input field is empty", newWindow)
+            show_error("The input field is empty", create_window)
 
-    newWindow = Toplevel(root)
-    newWindow.title('Create new database')
-    newWindow.geometry('250x150')
+    create_window = Toplevel(root)
+    create_window.title('Create new database')
+    create_window.geometry('250x150')
 
     name = StringVar()
-    title = Label(newWindow, text='Enter database name')
-    enter = Entry(newWindow, textvariable=name)
-    create = Button(newWindow, text='Create database', width=16, command=creation)
+    title = Label(create_window, text='Enter database name')
+    enter = Entry(create_window, textvariable=name)
+    create = Button(create_window, text='Create database', width=16, command=creation)
 
     title.place(anchor=CENTER, x=125, y=25)
     enter.place(anchor=CENTER, x=125, y=50)
@@ -109,6 +109,15 @@ def delete_database():
 
 def details():
     def open_table():
+        def refresh_table():
+            table.delete(*table.get_children())
+            rows.clear()
+            for item in cursor.execute("select * from get_{}('{}','')".format(table_list[subindex], db_list[index])):
+                rows.append(item)
+            for row in rows:
+                table.insert('', END, values=tuple(row))
+
+
         def search_in_table():
             if search_item.get():
                 try:
@@ -121,12 +130,121 @@ def details():
                 except:
                      show_error("Something goes wrong", table_window)
             else:
-                table.delete(*table.get_children())
-                for row in rows:
-                    table.insert('', END, values=tuple(row))
+                refresh_table()
                 
- 
+
+        def delete_rows():
+            try:
+                if table_list[subindex] == 'finances':
+                    messagebox.showerror("Error", "No rights to delete data from this table ({})".format(table_list[subindex]))
+                    return
+                for item in table.selection():
+                    item_text = table.item(item)['values']
+                    cursor.execute("select delete_rows('{}','{}','{}')".format(db_list[index], table_list[subindex], item_text[0]))
+                    print('DEBUG:: Delete row with condition {}'.format(item_text[0]))
+                refresh_table()
+            except:
+                messagebox.showerror("Error", "Error while deleting rows")
+
+        
+        def add_data_to_rows():
+            try:
+                def add_data_to_db():
+                    for item in range(len(text_variables)):
+                        if not text_variables[item].get():
+                            messagebox.showerror("Error", "The input field is empty - {}".format(headings[item]))
+                            return
+                    try:
+                        query_condition = ','.join("'''{}'''".format(i.get()) for i in text_variables)
+                        cursor.execute("select insert_rows('{}','{}',array[{}])".format(db_list[index], table_list[subindex], query_condition))
+                        #print("DEBUG:: query is {}".format(query_condition))
+                        refresh_table()
+                        add_window.destroy()
+                    except:
+                        messagebox.showerror("Error", "Error while additing rows")
+        
+
+                # Window setup
+                add_window = Toplevel(table_window)
+                add_window.title('{}'.format(table_list[subindex]))
+                add_window.geometry('300x350')
+
+                # Entry
+                entry_list = list()
+                text_variables = list()
+                labels_list = list()
+                for item in range(len(headings)):
+                    text_variables.append(StringVar())
+                    entry_list.append(Entry(add_window, textvariable=text_variables[item]))
+                    labels_list.append(Label(add_window, text=headings[item]))
+
+                # Buttons
+                add_current_data = Button(add_window, text='Add data', command=add_data_to_db)
+
+                # Places
+                place_under_entry = len(entry_list)
+                for item in range(len(entry_list)):
+                    labels_list[item].grid(column=0, row=item, pady=5)
+                    entry_list[item].grid(column=1, row=item, pady=5)
+                add_current_data.grid(column=1, row=place_under_entry, pady=5)
+
+                add_window.columnconfigure(0, weight=1)
+                add_window.columnconfigure(1, weight=1)
+                #add_window.rowconfigure(0, weight=1)
+            except:
+                show_error("Error while opening table '{}'".format(table_list[subindex]), add_window)
+
+
+        def update_data_in_rows():
+            try:
+                def update_data_in_db():
+                    for item in range(1, len(text_variables)):
+                        if not text_variables[item].get():
+                            messagebox.showerror("Error", "The input field is empty - {}".format(headings[item]))
+                            return
+                    query_condition = ','.join("'{}'".format(text_variables[i].get()) for i in range(1, len(text_variables)))
+                    cursor.execute("select update_rows('{}','{}','{}',array[{}])".format(db_list[index], table_list[subindex], data[0], query_condition))
+                    #print("DEBUG:: query is {}".format(query_condition))
+                    refresh_table()
+                    update_window.destroy()
+        
+
+                # Window setup
+                update_window = Toplevel(table_window)
+                update_window.title('{}'.format(table_list[subindex]))
+                update_window.geometry('300x350')
+
+                # Entry
+                data =  table.item(table.selection()[0])['values']
+                entry_list = list()
+                text_variables = list()
+                labels_list = list()
+                for item in range(len(headings)):
+                    text_variables.append(StringVar(update_window, data[item]))
+                    entry_list.append(Entry(update_window, textvariable=text_variables[item]))
+                    labels_list.append(Label(update_window, text=headings[item]))
+
+                # Buttons
+                add_current_data = Button(update_window, text='Update data', command=update_data_in_db)
+
+                # Places
+                place_under_entry = (len(entry_list) - 1)
+                for item in range(1, len(entry_list)):
+                    labels_list[item].grid(column=0, row=item-1, pady=5)
+                    entry_list[item].grid(column=1, row=item-1, pady=5)
+                add_current_data.grid(column=1, row=place_under_entry, pady=5)
+
+                update_window.columnconfigure(0, weight=1)
+                update_window.columnconfigure(1, weight=1)
+                #add_window.rowconfigure(0, weight=1)
+            except IndexError:
+                messagebox.showerror("Error", "No items selected")
+            except:
+                show_error("Error while opening table '{}'".format(table_list[subindex]), update_window)
+
+
         try:
+            # Window setup
             subindex = table_listbox.curselection()[0]
             table_window = Toplevel(details_window)
             table_window.title('{}'.format(table_list[subindex]))
@@ -146,7 +264,7 @@ def details():
             tree_frame = Frame(table_window)
 
             # Treeview
-            table = ttk.Treeview(master=tree_frame, show="headings", selectmode="browse", height=200)
+            table = ttk.Treeview(master=tree_frame, show="headings", selectmode="extended", height=200)
 
             # Scroll
             horScroll = ttk.Scrollbar(tree_frame)
@@ -164,9 +282,9 @@ def details():
 
             # Buttons
             search_button = Button(tree_frame, text='Search', command=search_in_table)
-            add_data = Button(tree_frame, text='Add data')
-            delete_data = Button(tree_frame, text='Delete data')
-            alter_data = Button(tree_frame, text='Alter data')
+            add_data = Button(tree_frame, text='Add data', command=add_data_to_rows)
+            delete_data = Button(tree_frame, text='Delete data', command=delete_rows)
+            update_data = Button(tree_frame, text='Update data', command=update_data_in_rows)
 
             # Entry
             search_item = StringVar()
@@ -201,7 +319,7 @@ def details():
             search_button.grid(column=1, row=5, sticky=N, pady=5, ipadx=0)
             add_data.grid(column=1, row=6, sticky=N, pady=5, ipadx=0)
             delete_data.grid(column=1, row=7, sticky=N, pady=5, ipadx=0)
-            alter_data.grid(column=1, row=8, sticky=N, pady=5, ipadx=0)
+            update_data.grid(column=1, row=8, sticky=N, pady=5, ipadx=0)
 
             table_window.columnconfigure(0, weight=1)
             table_window.rowconfigure(0, weight=1)
